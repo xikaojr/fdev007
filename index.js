23372cefdc0accaf1494026e2db98a7ff21903fa
vicentepaiva <vicentepaivadev@gmail.com>
2024-02-25 11:15:48 -0300

    refact taking off some unnecessary code

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-function createPhoneNumber(){
-    let phoneNumber = numbers;
-    let newPhoneNumber = `(${phoneNumber.slice(0,3).join('')}) ${phoneNumber.slice(3,6).join('')}-${phoneNumber.slice(6).join('')}`;
-    return newPhoneNumber;
+function createPhoneNumber(numbers){
+   
+    numbers = `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`;
+    return numbers;
 }
 
-console.log(createPhoneNumber(numbers)); 
+createPhoneNumber(numbers); // => (123) 456-7890
+console.log(createPhoneNumber(numbers))
