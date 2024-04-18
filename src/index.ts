5d6e552e894fa7a129b44c0e6f22fac17bc42e94
Ayrton Junior <xk.junior63@gmail.com>
2024-04-18 08:36:08 -0300

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,27 @@
 import express from 'express'
-import itensRouter from './routers/itens-router'
+// import itensRouter from './routers/itens-router'
 import cors from 'cors'
 
-// Porta do servidor
-const PORT = process.env.PORT || 4000
-// Host do servidor
-const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
-// App Express
+const PORT = 8000
 const app = express()
-// Endpoint raiz
-// JSON
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// Rotas
 app.get('/', (req, res) => {
   res.send('Bem-vindo!')
 })
-// Cors
 app.use(cors({
   origin: '*'
 }))
+// app.use('/api', itensRouter)
+app.use('/console', (req, res) => {
+  console.log(req.headers)
+  console.log(req.body)
+})
 
-//ROTAS
-app.use('/api', itensRouter)
-
-// Resposta padrão para quaisquer outras requisições:
 app.use((req, res) => {
   res.status(404).send('Rota não encontrada!')
 })
 // Inicia o sevidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
+  console.log(`Servidor rodando com sucesso ${PORT}`)
 })
